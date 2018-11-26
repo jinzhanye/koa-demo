3427ed7d4959c16f27d0369368f9c536e8402f59
jinzhanye <jinzhan.ye@foxmail.com>
2018-11-26 20:42:18 +0800

    使用post中间件

--- post-demo.js
+++ post-demo.js
@@ -1,5 +1,9 @@
 const Koa = require('koa');
+const bodyparser = require('koa-bodyparser');
 const app = new Koa();
+
+app.use(bodyparser());
+
 app.use(async (ctx) => {
     //当请求时GET请求时，显示表单让用户填写
     if (ctx.url === '/' && ctx.method === 'GET') {
@@ -16,40 +20,13 @@ app.use(async (ctx) => {
             </form>`;
         //当请求时POST请求时
     } else if (ctx.url === '/' && ctx.method === 'POST') {
-        let parseData = await parsePostData(ctx);
+        let parseData = ctx.request.body;
         ctx.body = parseData;
     } else {
         //其它请求显示404页面
         ctx.body = '<h1>404!</h1>';
     }
 });
-
-function parsePostData(ctx) {
-    return new Promise((resolve, reject) => {
-        try {
-            let postdata = '';
-            ctx.req.on('data', (data) => {
-                postdata += data;
-            });
-            ctx.req.addListener('end', () => {
-                let parseData = parseQueryStr(postdata);
-                resolve(parseData);
-            })
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
-
-function parseQueryStr(queryStr) {
-    let queryData = {};
-    let queryStrList = queryStr.split('&');
-    Object.entries(queryStrList).forEach(([idx, queryStr]) => {
-        let itemList = queryStr.split('=');
-        queryData[itemList[0]] = decodeURIComponent(itemList[1]);
-    });
-    return queryData
-}
 
 app.listen(8000);
 console.log('listen port 8000 ....');
